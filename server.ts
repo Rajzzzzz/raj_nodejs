ce748db0f8f308ca61fcef7a572d61b9e69c2389
Rajzzzzz <rajeshwarraj800@gmail.com>
2024-02-25 21:56:54 +0530

    aws server

--- server.ts
+++ server.ts
@@ -5,7 +5,7 @@ import * as express from "express";
 let app:any = express();
 
 //create the get request
-app.get("/",(_req:any,res:any)=>{
+app.get("/demo1",(_req:any,res:any)=>{
     res.status(200).json({"message":"welcome to demo1 request"});
 })
 
